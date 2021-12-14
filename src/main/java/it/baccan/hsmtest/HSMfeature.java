585a549e8add27a592012f0ad63a09c3ecd687db
Matteo Baccan <matteo.baccan@gmail.com>
2021-12-14 12:09:13 +0100

    Update HSMfeature.java

--- src/main/java/it/baccan/hsmtest/HSMfeature.java
+++ src/main/java/it/baccan/hsmtest/HSMfeature.java
@@ -9,6 +9,13 @@ import com.ncipher.km.nfkm.Key;
 import com.ncipher.km.nfkm.Module;
 import com.ncipher.km.nfkm.SecurityWorld;
 import com.ncipher.nfast.NFException;
+import com.ncipher.nfast.connect.NFConnection;
+import com.ncipher.nfast.marshall.M_Cmd;
+import com.ncipher.nfast.marshall.M_Cmd_Args_NVMemList;
+import com.ncipher.nfast.marshall.M_Cmd_Args_NoOp;
+import com.ncipher.nfast.marshall.M_Command;
+import com.ncipher.nfast.marshall.M_Reply;
+import com.ncipher.nfast.marshall.M_Status;
 import java.util.Arrays;
 import lombok.extern.slf4j.Slf4j;
 
@@ -23,12 +30,15 @@ public class HSMfeature {
         log.info("Start test");
 
         try {
+            // Connesione a security World
             SecurityWorld securityWorld = new SecurityWorld(new ConsoleCallBack());
 
+            // Elenco chiavi
             Key[] keys = securityWorld.listKeys("simple");
             log.info("Keys:" + keys.length);
             Arrays.asList(keys).forEach(key -> log.info(key.toString()));
 
+            // Elenco moduli
             Module[] modules = securityWorld.getModules();
             log.info("Modules:" + modules.length);
             Arrays.asList(modules).forEach(module -> log.info(module.toString()));
@@ -36,11 +46,36 @@ public class HSMfeature {
                 log.info("Security world has key recovery enabled.");
             }
 
+            // Connessione diretta per comandi
+            NFConnection nFConnection = new NFConnection();
+
+            // NoOp
+            M_Cmd_Args_NoOp commandArgsNoOp = new M_Cmd_Args_NoOp(modules[0].getID());
+            M_Command mCommandNoOp = new M_Command(M_Cmd.NoOp, 0, commandArgsNoOp);
+            M_Reply mReplyNoOp = sendCommand(nFConnection, mCommandNoOp);
+            log.info("NoOp [{}]", mReplyNoOp);
+
+            // MeMList
+            M_Cmd_Args_NVMemList commandArgs = new M_Cmd_Args_NVMemList(modules[0].getID(), 0);
+            M_Command mCommand = new M_Command(M_Cmd.NVMemList, 0, commandArgs);
+            M_Reply mReply = sendCommand(nFConnection, mCommand);
+            log.info("NVMMemList [{}]", mReply);
         } catch (NFException nFException) {
-            log.info(nFException.getMessage());
+            log.info("NFException", nFException);
         }
 
         log.info("End test");
+    }
+
+    private static M_Reply sendCommand(final NFConnection nFConnection, final M_Command mCommand) throws NFException {
+        log.info("Command [{}]", mCommand.toString());
+        M_Reply reply = nFConnection.transact(mCommand);
+        if (reply.status == M_Status.OK) {
+            log.info("Reply [{}]", reply.reply);
+        } else {
+            throw new NFException(M_Status.toString(reply.status));
+        }
+        return reply;
     }
 
 }
